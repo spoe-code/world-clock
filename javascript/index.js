7248e253ccfcf3e702eb191b855efb5e1573f2c6
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-04-03 20:52:13 -0400

    added city ID and adjust JS to show selected city's current time

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,9 @@ function updateTime() {
   let losAngelesTime = moment().tz("America/Los_Angeles");
 
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
 
   let sydneyElement = document.querySelector("#sydney");
   let sydneyDateElement = sydneyElement.querySelector(".date");
@@ -13,8 +15,32 @@ function updateTime() {
   let sydneyTime = moment().tz("Australia/Sydney");
 
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+  <h2>${cityName}</h2>
+  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
